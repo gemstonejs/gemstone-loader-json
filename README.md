9bee3ed196e825866e588244df65d5973823a063
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 14:28:06 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gemstone-loader-json
-Webpack Loader for JSON
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Loader JSON
+====================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-loader-json.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-loader-json.png" alt=""/>
+
+About
+-----
+
+This is the Webpack Loader for JSON files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-loader-json.js
@@ -0,0 +1,34 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const loaderUtils = require("loader-utils")
+
+/*  the exported Webpack loader function  */
+module.exports = function (content) {
+    /*  determine Webpack loader query parameters  */
+    const options = Object.assign({}, {
+    }, loaderUtils.getOptions(this), this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null)
+    void (options)
+
+    /*  indicate to Webpack that our results are
+        fully deterministic and can be cached  */
+    this.cacheable(true)
+
+    /*  parse JSON  */
+    try {
+        content = JSON.parse(content)
+    }
+    catch (ex) {
+        this.emitError(`gemstone-loader-json: [JSON]: ERROR: ${ex}`)
+    }
+
+    /*  export result as a JavaScript string  */
+    content = `module.exports = ${JSON.stringify(content)}`
+
+    return content
+}
+
